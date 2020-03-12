34f57e4cc4c6b1f10a497d62dc4a63dca5d2bfa2
linikerj <linikerenem@gmail.com>
2020-03-12 00:46:06 -0300

    'exe18'

--- GeekUniversity/GeekExercise/Section 5/exe18.py
+++ GeekUniversity/GeekExercise/Section 5/exe18.py
@@ -4,11 +4,33 @@ uma das opções e o seu programa então pede dois valores numéricos e realiza 
 """
 
 while True:
+    def somar(number_1, number_2):
+        soma = number_1 + number_2
+        print(f"\n A soma dos números é : {round(soma, 2)}")
+    
+    def subtrair(number_1, number_2):
+        subtracao = number_1 - number_2
+        print(f"\n A subtração dos números é : {round(subtracao, 2)}")
+    
+    def multiplicar(number_1, number_2):
+        multiplicacao = number_1 * number_2
+        print(f"\n A multiplicação dos números é : {round(multiplicacao, 2)}")
+
+    def dividir(number_1, number_2):
+        try:
+            divisao = number_1 / number_2
+            print(f"\n A divisão dos números é : {round(divisao, 2)}")
+        except:
+            print("Dividir por Zero não é permitido")
+        finally:
+            pass
+
+
     print(""" ***************** MENU ******************* \n
     [1] - [    Somar    ]\t \n
     [2] - [   Subtrair  ]\t \n
     [3] - [ Multiplicar ]\t \n
-    [4] - [   Divisão   ]\t \n
+    [4] - [   Dividir   ]\t \n
 *******************************************
     """)
     options = input("Digite uma opção válida de 1 a 4 : ")
@@ -17,4 +39,25 @@ while True:
     except:
         print("O valor deve ser um número.")
     finally:
-        pass
+        if options < 1 or options > 4:
+            print("DIGITO INVÁLIDO, DEVE ESTAR ENTRE 1 E 4. \n \n")
+        else:
+            number_1 = input("\nDigite o primeiro número : ")
+            number_2 = input("\nDigite o segundo número : ")
+            try:
+                number_1, number_2 = float(number_1), float(number_2)
+            except:
+                print("Digito inválido.")    
+            finally:
+                switcher = {
+                    1: somar(number_1, number_2),
+                    2: subtrair(number_1, number_2),
+                    3: multiplicar(number_1, number_2),
+                    4: dividir(number_1, number_2),
+                }
+
+                if options in switcher:
+                    switcher[options]
+                else:
+                    print("Erro, número inexistente ")
+                print("To aqui")
