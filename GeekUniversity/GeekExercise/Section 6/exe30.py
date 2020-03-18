09c96ecbdb698ac2fcf5ab3c0047d9e2badf5654
linikerj <linikerenem@gmail.com>
2020-03-18 16:58:25 -0300

    'start-exe-30'

---
+++ GeekUniversity/GeekExercise/Section 6/exe30.py
@@ -0,0 +1,6 @@
+"""
+Faça um programa para calcular as seguintes sequências:
+        1 + 2 + 3 + 4 + 5 + ... + n
+        1 - 2 + 3 - 4 + 5 + ... + (2n - 1)
+        1 + 3 + 5 + 7 + ... + (2n - 1)
+"""
