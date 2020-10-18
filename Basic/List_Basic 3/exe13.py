bd64cd938ce404da2458da7f08a39b9dce4abf50
Liniker Oliveira <linikerenem@gmail.com>
2020-10-18 04:14:27 -0300

    fixing the broken old code

--- Basic/List_Basic 3/exe13.py
+++ Basic/List_Basic 3/exe13.py
@@ -1,6 +1,6 @@
 #Escreva um programa que pergunte o valor inicial de uma dívida e o juros mensal.
 #Pergunte também o valor mensal que será pago. Imprima o número de meses para
-#que a dívida seja paga, o total pago e o total de juros pago.
+#que a dívida seja paga, o total pago e o total de juros pago .
 
 initial_value = float(input("Initial debt value : "))# 100
 rate = float(input('Input the monthly tax rate in % [without %] : ')) # 1
