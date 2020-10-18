3c26b1599e2eacf14692a5cb4cd47ba3e5f1e88b
Liniker Oliveira <linikerenem@gmail.com>
2020-10-18 04:11:33 -0300

    fixing a broken old code

--- Basic/List_Basic 3/exe13.py
+++ Basic/List_Basic 3/exe13.py
@@ -2,20 +2,15 @@
 #Pergunte também o valor mensal que será pago. Imprima o número de meses para
 #que a dívida seja paga, o total pago e o total de juros pago.
 
-divida = float(input('Valor da divida : '))
-taxa = float(input('Digite a taxa do juros mesal em % [sem o %] : '))
-valor_pgt = float(input('Valor pago mensalmente : '))
+initial_value = float(input("Initial debt value : "))# 100
+rate = float(input('Input the monthly tax rate in % [without %] : ')) # 1
+payment = float(input('Monthly payment : ')) # 25
 
-meses_pgt = divida / valor_pgt
-meses_pgt = int(meses_pgt)
-print(f' Número de mêses para pagar a dívida : {meses_pgt} ')
-print(f' Total  pago : {divida:.2f} \n')
+months_debt = initial_value / payment
+interest = initial_value * (rate/100) * months_debt
+final_value = initial_value + interest
 
-cont = 0
-for meses_pgt in enumerate(range(1, meses_pgt)):
-    juros = divida * (taxa/100)
-    divida = divida + juros - valor_pgt
-    cont += juros
-print(f' Total pago em juros : {cont:.2f}')
-
-    
+print(f"You'll pay this debt during {int(months_debt)} month(s).")
+print(f"You'll pay an interest of U$ {interest:.2f} for the debt of {initial_value}")
+print(f"After {int(months_debt)} month(s), \
+you'll have paid U$ {final_value:.2f}")
